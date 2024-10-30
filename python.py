d97467a1c91da279c98855b76dd1b7145fe0ab77
dajari1 <dajtech88@gmail.com>
2024-10-30 12:35:14 -0400

    i just fixed git

---
+++ python.py
@@ -0,0 +1,32 @@
+a=1
+b=2
+c=a+b
+print(c)
+
+a=20
+b=10
+c=a-b
+print(c)
+
+a=50
+b=60
+c=a*b
+print("this is the answer",c)
+
+a=50
+b=60
+c=40
+d=a*b*c
+f=d-100
+print("this is the anser",f)
+g=f/50
+print('this is the answer', g)
+
+if g >100:
+    print('it is greater than 100')
+    h=g*1000/50
+    print(h)
+else:
+    print('less than 100')
+    
+    
